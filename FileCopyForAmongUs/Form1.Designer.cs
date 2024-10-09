d9bbe68156b58cf62b823b28ea44fe14abe1fd9f
Jake Garner <7588861+Fixxzer@users.noreply.github.com>
2024-10-08 22:10:09 -0600

    Add project files.

---
+++ FileCopyForAmongUs/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace FileCopyForAmongUs
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            button1 = new Button();
+            progressBar1 = new ProgressBar();
+            label1 = new Label();
+            label2 = new Label();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(3, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(528, 530);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(655, 3);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(530, 530);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Bottom;
+            button1.BackColor = SystemColors.ControlDark;
+            button1.Font = new Font("Segoe UI", 14F);
+            button1.ForeColor = SystemColors.ControlText;
+            button1.Location = new Point(495, 640);
+            button1.Name = "button1";
+            button1.Size = new Size(204, 35);
+            button1.TabIndex = 2;
+            button1.Text = "Download";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            progressBar1.Location = new Point(199, 690);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(811, 23);
+            progressBar1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Bottom;
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F);
+            label1.Location = new Point(583, 716);
+            label1.Name = "label1";
+            label1.Size = new Size(32, 21);
+            label1.TabIndex = 4;
+            label1.Text = "0%";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top;
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15F);
+            label2.Location = new Point(569, 22);
+            label2.Name = "label2";
+            label2.Size = new Size(65, 28);
+            label2.TabIndex = 5;
+            label2.Text = "label2";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // backgroundWorker1
+            // 
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.DoWork += backgroundWorker1_DoWork;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            tableLayoutPanel1.ColumnCount = 3;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 45F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 45F));
+            tableLayoutPanel1.Controls.Add(pictureBox1, 0, 0);
+            tableLayoutPanel1.Controls.Add(pictureBox2, 2, 0);
+            tableLayoutPanel1.Location = new Point(12, 69);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Size = new Size(1188, 536);
+            tableLayoutPanel1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1228, 786);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(progressBar1);
+            Controls.Add(button1);
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "Form1";
+            Text = "File Download";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            tableLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Button button1;
+        private ProgressBar progressBar1;
+        private Label label1;
+        private Label label2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private TableLayoutPanel tableLayoutPanel1;
+    }
+}
---
+++ FileCopyForAmongUs/Form1.cs
@@ -0,0 +1,143 @@
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace FileCopyForAmongUs
+{
+    public partial class Form1 : Form
+    {
+        private static readonly Random Rand = new();
+
+        private const int MaxValue = 100;
+        private const int Fast = 100;
+        private const int Regular = 200;
+        private const int Slow = 1500;
+        
+        private int randomError;
+        private int randomErrorVal;
+        private int currRandom;
+        private bool hitRandom;
+        private int errorTicks;
+        private int slowRandom;
+        private int slowRandomVal;
+        private int fastRandom;
+        private int fastRandomVal;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 100;
+
+            label2.Text = string.Empty;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            randomError = Rand.Next(0, MaxValue);
+            randomErrorVal = Rand.Next(0, 5);
+            slowRandom = Rand.Next(0, (MaxValue / 2) - 10);
+            slowRandomVal = Rand.Next(1, 10);
+            fastRandom = Rand.Next(MaxValue / 2, MaxValue);
+            fastRandomVal = Rand.Next(0, 30);
+
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            UpdateProgressBar1Safe(0);
+
+            for (var i = 0; i < MaxValue; i++)
+            {
+                Debug.WriteLine($"Iteration: {i}");
+                if (i == randomError && !hitRandom)
+                {
+                    Debug.WriteLine($"Error: {randomError}");
+                    errorTicks = 0;
+                    UpdateLabel2Safe("Error!!!");
+
+                    // Reset the current count
+                    i = Rand.Next(5, MaxValue - 10);
+
+                    if (currRandom == randomErrorVal)
+                    {
+                        hitRandom = true;
+                    }
+
+                    randomError = Rand.Next(0, MaxValue);
+                    currRandom++;
+                }
+
+                int timeDelay;
+                if (i >= slowRandom && i <= slowRandom + slowRandomVal)
+                {
+                    Debug.WriteLine($"Slow: {slowRandom} - {slowRandom + slowRandomVal}");
+                    timeDelay = Slow;
+                }
+                else if (i >= fastRandom && i <= fastRandom + fastRandomVal)
+                {
+                    Debug.WriteLine($"Fast: {fastRandom} - {fastRandom + fastRandomVal}");
+                    timeDelay = Fast;
+                }
+                else
+                {
+                    Debug.WriteLine("Regular");
+                    timeDelay = Regular;
+                }
+
+                // Remove "Error!!!" text
+                if (errorTicks >= 5)
+                {
+                    UpdateLabel2Safe(string.Empty);
+                    errorTicks = 0;
+                }
+
+                UpdateLabel1Safe($@"{i}%");
+                UpdateProgressBar1Safe(i);
+                errorTicks++;
+                Thread.Sleep(timeDelay);
+            }
+
+            UpdateProgressBar1Safe(100);
+            UpdateLabel1Safe("100%");
+            MessageBox.Show("Download Complete!");
+        }
+
+        private void UpdateLabel1Safe(string text)
+        {
+            if (label1.InvokeRequired)
+            {
+                label1.Invoke(new MethodInvoker(delegate { label1.Text = text; }));
+            }
+            else
+            {
+                label1.Text = text;
+            }
+        }
+
+        private void UpdateLabel2Safe(string text)
+        {
+            if (label2.InvokeRequired)
+            {
+                label2.Invoke(new MethodInvoker(delegate { label2.Text = text; }));
+            }
+            else
+            {
+                label2.Text = text;
+            }
+        }
+
+        private void UpdateProgressBar1Safe(int value)
+        {
+            if (progressBar1.InvokeRequired)
+            {
+                progressBar1.Invoke(new MethodInvoker(delegate { progressBar1.Value = value; }));
+            }
+            else
+            {
+                progressBar1.Value = value;
+            }
+        }
+    }
+}
